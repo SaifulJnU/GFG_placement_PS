836d2ed715b50a747c552be5879873068694f9c0
Saiful Islam <saiful2561998@gmail.com>
2020-07-23 10:39:38 +0600

    Addition Under Modulo

---
+++ Mathematics/Addition Under Modulo.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int SumUnderModulo(long long a, long long b)
+{
+    ///(a+b)%m=(a%m+b%m)%m where m can be 100000007
+    long long M=1000000007;
+    return (a%M+b%M)%M;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long a,b;
+        cin>>a>>b;
+
+        long long result = SumUnderModulo(a,b);
+        cout<<result<<endl;
+    }
+    return 0;
+}
